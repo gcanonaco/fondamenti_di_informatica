df0b1a1abaa23b93f5cb1c01219b7ad346ce17e8
gcanonaco <canonacogiuseppe@gmail.com>
2018-10-03 10:08:50 +0200

    soluzioni lab 1

---
+++ Lab1/soluzioni/numeri_primi/main.c
@@ -0,0 +1,24 @@
+//Prova a eseguire, ad esempio, il fattoriale di 17.
+#include <stdio.h>
+
+int main(){
+    int fatt=1;
+    int a,i,flag=1;
+    printf("Inserisci un numero: ");
+    scanf("%d", &a);
+    for(i=2; i<a && flag; i++)
+        if(a % i == 0)
+            flag = 0;
+    if (flag)
+        printf("%d è primo!\n",a);
+    else
+        printf("%d non è primo!\n",a);
+    printf("Vuoi eseguire anche il fattoriale? [inserisci 0 per non eseguirlo!]");
+    scanf("%d",&i);
+    if(i){
+        for(i=1; i<=a; i++)
+            fatt *= i;
+        printf("Il fattoriale di %d è: %ld",a,fatt);
+    }
+    return 0;
+}
