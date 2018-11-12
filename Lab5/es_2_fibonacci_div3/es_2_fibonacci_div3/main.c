ce8c982934e0a6ceb2023fd17faeddd0d44680ee
ing.conti <ingconti@MacBookPro-32MB.homenet.telecomitalia.it>
2018-11-12 20:24:52 +0100

    cleaned up dirs...

--- Lab5/es_2_fibonacci_div3/es_2_fibonacci_div3/main.c
+++ Lab5/es_2_fibonacci_div3/es_2_fibonacci_div3/main.c
@@ -8,8 +8,36 @@
 
 #include <stdio.h>
 
+
+// per prima cosa fibonacci:
+
+int Fibonacci(int N){
+    if (N < 0) return -1; // non definita x N negativo
+    
+    // scatiamo casi "vicini" all' 1 che NON hanno precedenti:
+    if (N == 0)
+        return 0;
+    if (N == 1)
+        return 1;
+    
+    return Fibonacci(N-1) + Fibonacci(N-2);
+    
+}
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
+   
+    
+    // per comodita mettiano n fisso:
+    int n = 3;
+    
+    int i = 2;// parto dal 2, potre partire anche con il primo numero che so divisibile x 3 ...
+    int cont_divisibili = 0;
+    while (cont_divisibili<n){
+        int fib = Fibonacci(i);
+        if ( fib % 3 == 0){
+            cont_divisibili+=1;
+            printf("%d divisibile x 3\n", fib);
+        }
+        i++;
+    }
     return 0;
 }
---
+++ Lab5/es_4_matrici_di_binari/es_4_matrici_di_binari/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  es_4_matrici_di_binari
+//
+//  Created by ing.conti on 12/11/2018.
+//  Copyright © 2018 ing.conti. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
